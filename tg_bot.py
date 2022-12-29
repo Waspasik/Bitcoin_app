2e759c71f67819063ec39658fd7f93358abe4508
Waspasik <waspasik97@gmail.com>
2022-12-29 19:22:41 +0300

    Added callback query func and balance receipt func

--- tg_bot.py
+++ tg_bot.py
@@ -14,7 +14,8 @@ def start_message(message):
     transaction = types.KeyboardButton('Перевод')
     history = types.KeyboardButton('История')
     markup.add(wallet, transaction, history)
-    bot.send_message(message.chat.id, 'у меня ты можешь хранить и отправлять биткоины', reply_markup=markup)
+    text = 'У меня ты можешь хранить и отправлять биткоины'
+    bot.send_message(message.chat.id, text, reply_markup=markup)
     # print(message.from_user.to_dict())
 
 
@@ -43,7 +44,7 @@ def history(message):
     btn1 = types.KeyboardButton('Меню')
     markup.add(btn1)
     transactions = ['1', '2', '3']
-    text = f'Ваши транзакции{transactions}'
+    text = f'Ваши транзакции {transactions}'
     bot.send_message(message.chat.id, text, reply_markup=markup)
 
 
@@ -75,9 +76,70 @@ def all_users(message):
     text = f'Пользователи:'
     inline_markup = types.InlineKeyboardMarkup()
     for user in users:
-        inline_markup.add(types.InlineKeyboardButton(text=f'Юзер: {user["name"]}',
+        inline_markup.add(types.InlineKeyboardButton(text=f'Пользователь: {user["name"]}',
                                                              callback_data=f"user_{user['id']}"))
     bot.send_message(message.chat.id, text, reply_markup=inline_markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    query_type = call.data.split('_')[0]
+    if query_type == 'user':
+        user_id = call.data.split('_')[1]
+        inline_markup = types.InlineKeyboardMarkup()
+        for user in users:
+            if str(user['id']) == user_id:
+                back = types.InlineKeyboardButton(text='Назад', callback_data='users')
+                delete_user = types.InlineKeyboardButton(text='Удалить юзера', callback_data=f'delete_user_{user_id}')
+                inline_markup.add(back, delete_user)
+                bot.edit_message_text(text=f'Данные по пользователю:\n'
+                                           f'ID: {user["id"]}\n'
+                                           f'Имя: {user["name"]}\n'
+                                           f'Ник: {user["nick"]}\n'
+                                           f'Баланс: {user["balance"]}',
+                                      chat_id=call.message.chat.id,
+                                      message_id=call.message.message_id,
+                                      reply_markup=inline_markup)
+                print('Запрошен ', user)
+                break
+    if query_type == 'users':
+        inline_markup = types.InlineKeyboardMarkup()
+        for user in users:
+            show_user = types.InlineKeyboardButton(text=f'Пользователь: {user["name"]}',
+                                                   callback_data=f"user_{user['id']}")
+            inline_markup.add(show_user)
+            bot.edit_message_text(text='Пользователи:',
+                                 chat_id=call.message.chat.id,
+                                 message_id=call.message.message_id,
+                                 reply_markup=inline_markup)
+    if query_type == 'delete' and call.data.split('_')[1] == 'user':
+        user_id = int(call.data.split('_')[2])
+        for index, user in enumerate(users):
+            print(user['name'])
+            if user['id'] == user_id:
+                print(f'Удален пользователь: {users[index]}')
+                users.pop(index)
+        inline_markup = types.InlineKeyboardButton
+        for user in users:
+            show_user = types.InlineKeyboardButton(text=f'Пользователь: {user["name"]}',
+                                                   callback_data=f"user_{user['id']}")
+            bot.edit_message_text(text='Пользователи:',
+                                 chat_id=call.message.chat.id,
+                                 message_id=call.message.message_id,
+                                 reply_markup=inline_markup)
+
+
+@bot.message_handler(func=lambda message: message.from_user.id == config.tg_admin_id and message.text == 'Общий баланс')
+def total_balance(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    menu = types.KeyboardButton('Меню')
+    admin = types.KeyboardButton('Админ')
+    markup.add(menu, admin)
+    balance = 0.0
+    for user in users:
+        balance += user['balance']
+    text = f'Общий баланс: {balance}'
+    bot.send_message(message.chat.id, text, reply_markup=markup)
 
 
 bot.infinity_polling()
